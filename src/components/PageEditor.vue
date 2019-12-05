5bf87caec89204652c3c0f3dcd6b9f78d7b11c14
Yan <yanyuxuan@nobook.com>
2019-12-06 03:05:22 +0800

    feat: 创建编辑器layout

---
+++ src/components/PageEditor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <slot name="header"></slot>
+    </div>
+    <div class="container-nav">
+      <slot name="nav"></slot>
+    </div>
+    <div class="container-main">
+      <slot name="main"></slot>
+    </div>
+    <div class="container-tools">
+      <slot name="tools"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import '../css/reset.css';
+export default {
+    name: 'PageEditor',
+    data () {
+        return { msg: 'Editor Page' }
+    }
+}
+</script>
+
+<style scoped>
+.container-header {
+    height: 5vh;
+    background-color: aqua;
+}
+.container-nav {
+    height: 20vh;
+    background-color: burlywood;
+}
+.container-main {
+    height: 75vh;
+    background-color: darkolivegreen;
+}
+.container-tools {
+    position: fixed;
+    width: 5vw;
+    height: 50vh;
+    bottom: 0px;
+    background-color: turquoise;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import PageEditor from '@/components/PageEditor'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/editor',
+      name: 'PageEditor',
+      component: PageEditor
+    },
     {
       path: '/',
       name: 'HelloWorld',
