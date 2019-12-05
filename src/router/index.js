a56a06a6def0ffdf8c2345cb27eb0975a06c460f
Yan <yanyuxuan@nobook.com>
2019-12-06 03:07:25 +0800

    delete: 删除vue默认页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import PageEditor from '@/components/PageEditor'
 
 Vue.use(Router)
@@ -8,14 +7,9 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/editor',
+      path: '/',
       name: 'PageEditor',
       component: PageEditor
-    },
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
     }
   ]
 })
