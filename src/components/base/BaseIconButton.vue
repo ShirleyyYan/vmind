544f948ffd6f9b135374f58f275c40eadc2e28cf
Yan <yanyuxuan@nobook.com>
2019-12-07 19:54:03 +0800

    feat: 添加按钮和按钮分组组件

---
+++ src/components/base/BaseIconButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+    type="button"
+    v-bind:style="iconSize"
+    v-bind:title="title"
+    v-bind:alt="title"
+    class="container-icon"
+    ></div>
+</template>
+
+<script>
+export default {
+    name: 'BaseIconButton',
+    props: {
+        img: String,
+        size: {
+            type: Number,
+            default: 40
+        },
+        onClick: String,
+        isActive: Boolean,
+        title: String
+    },
+    computed: {
+        iconSize: function () {
+            return {
+                width: this.size + 'px',
+                height: this.size + 'px'
+            };
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container-icon {
+    background-color:lightblue;
+    display: inline-block;
+}
+.container-icon:hover {
+    background-color: blue;
+}
+</style>
