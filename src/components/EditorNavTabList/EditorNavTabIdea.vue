0033fbd26efb1582663e4698758d9b8f1a7d5dc7
Yan <yanyuxuan@nobook.com>
2019-12-07 23:36:31 +0800

    feat: 添加文字按钮

--- src/components/EditorNavTabList/EditorNavTabIdea.vue
+++ src/components/EditorNavTabList/EditorNavTabIdea.vue
@@ -4,17 +4,22 @@
         <base-icon-button class="icon-item" v-bind:size="30" title="Add Node"></base-icon-button>
         <base-icon-button class="icon-item" v-bind:size="30" title="Delete Node"></base-icon-button>
     </editor-nav-icon-group>
+    <editor-nav-icon-group>
+        <base-icon-text title='Insert Lower Level'></base-icon-text>
+    </editor-nav-icon-group>
     </div>
 </template>
 
 <script>
-import EditorNavIconGroup from './EditorNavBase/EditorNavIconGroup';
+import EditorNavIconGroup from './EditorNavBase/EditorNavIconGroup.vue';
 import BaseIconButton from '../base/BaseIconButton.vue';
+import BaseIconText from '../base/BaseIconButtonText.vue';
 export default {
     name: 'EditorNavTapIdea',
     components: {
         BaseIconButton,
-        EditorNavIconGroup
+        EditorNavIconGroup,
+        BaseIconText
     }
 }
 </script>
---
+++ src/components/base/BaseIconButtonText.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container-icon-text"
+    v-bind:style="btnStyle"
+    >
+    {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseButtonText',
+    props: {
+        img: String,
+        imgSize: {
+            type: Number,
+            default: 40
+        },
+        height: {
+            type: Number,
+            default: 40
+        },
+        width: {
+            type: Number,
+            default: 200
+        },
+        onClick: String,
+        isActive: {
+            type: Boolean,
+            default: true
+        },
+        title: String
+    },
+    computed: {
+        btnStyle: function () {
+            return {
+                'min-width': `${this.width}px`,
+                height: `%{this.height}px`
+            };
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.container-icon-text {
+    display: inline-block;
+    font-size: 1.8vh;
+    border: 1px #EEEEEE solid;
+    border-radius: 5px;
+    line-height: 2.5vh;
+    padding-left: 4vh;
+}
+.container-icon-text:hover {
+    border-color: #CCCCCC;
+}
+</style>
